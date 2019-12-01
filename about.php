8a6a294b4efaa15852fa816d480840f620ce3349
Patrick Roelofs <stolenbows@gmail.com>
2019-12-01 17:05:57 +0100

    head import

--- about.php
+++ about.php
@@ -2,22 +2,7 @@
 <html lang="en">
 
 <head>
-	<title>About | Patrick Roelofs</title>
-
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-	<meta name="author" content="Patrick Roelofs">
-	<meta name="description" content="Patrick Roelofs is student, designer and developer in the Netherlands.">
-	<meta name="keywords" content="Developer, Designer, Interaction, UX, Webdeveloper, HTML, CSS, PHP, JS" >
-	
-	<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
-	<link rel="manifest" href="images/favicon/site.webmanifest">
-
-	<link rel="stylesheet" type="text/css" href="css/stylesheet-min.css">
+	<?php include('imports/head.php'); ?>
 </head>
 
 <body>
