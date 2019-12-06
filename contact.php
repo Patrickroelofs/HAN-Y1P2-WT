286dab2c5b365908ad1dbb9ec4af89e3e940f45b
Patrick Roelofs <stolenbows@gmail.com>
2019-12-06 20:37:15 +0100

    initial imports and fixes for renamed php files

--- contact.php
+++ contact.php
@@ -6,36 +6,7 @@
 </head>
 
 <body>
-
-
-	<nav>
-		<div class="container container--large nav">
-			<input class="nav--icon" type="checkbox" id="menu-btn" />
-			<label class="menu--icon" for="menu-btn">
-				<svg viewbox="0 0 100 100" width="64">
-					<path class="line top"
-						d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20">
-					</path>
-					<path class="line middle" d="m 30,50 h 40"></path>
-					<path class="line bottom"
-						d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"></path>
-				</svg>
-			</label>
-			<div class="nav__logo">
-				<a href="index.html">
-					<h1>Patrick Roelofs</h1>
-				</a>
-			</div>
-			<div class="nav__navigation mobile--hidden">
-				<ul>
-					<li><a href="index.html" class="nav__link">Home</a></li>
-					<li><a href="about.html" class="nav__link">About</a></li>
-					<li><a href="projects.html" class="nav__link">Projects</a></li>
-					<li><a href="contact.html" class="nav__link nav--active">Contact</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+	<?php include('imports/navigation.php'); ?>
 
 	<header class="header">
 		<div class="container container--large center">
@@ -90,46 +61,7 @@
 		</div>
 	</main>
 
-	<footer class="footer">
-		<div class="footer__contact">
-			<p class="footer__title">Send me an email.</p>
-			<p class="footer__subtitle">It's easier than a form!</p>
-			<a href="mailto:contact@patrickroelofs.com" class="button button--black button--large">
-				contact@patrickroelofs.com
-			</a>
-		</div>
-		<div class="footer__social">
-			<ul>
-				<li>
-					<a target="_blank" href="https://twitter.com/downmetpatrick">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24">
-							<path
-								d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-						</svg>
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://www.linkedin.com/in/patrick-roelofs/">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24">
-							<path
-								d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
-						</svg>
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://www.behance.net/patrickroelofs">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24">
-							<path
-								d="M22 7h-7v-2h7v2zm1.726 10c-.442 1.297-2.029 3-5.101 3-3.074 0-5.564-1.729-5.564-5.675 0-3.91 2.325-5.92 5.466-5.92 3.082 0 4.964 1.782 5.375 4.426.078.506.109 1.188.095 2.14h-8.027c.13 3.211 3.483 3.312 4.588 2.029h3.168zm-7.686-4h4.965c-.105-1.547-1.136-2.219-2.477-2.219-1.466 0-2.277.768-2.488 2.219zm-9.574 6.988h-6.466v-14.967h6.953c5.476.081 5.58 5.444 2.72 6.906 3.461 1.26 3.577 8.061-3.207 8.061zm-3.466-8.988h3.584c2.508 0 2.906-3-.312-3h-3.272v3zm3.391 3h-3.391v3.016h3.341c3.055 0 2.868-3.016.05-3.016z" />
-						</svg>
-					</a>
-				</li>
-			</ul>
-		</div>
-		<div class="footer__copyright">
-			<p>Handcrafted by Patrick Roelofs 2019</p>
-		</div>
-	</footer>
+	<?php include('imports/footer.php'); ?>
 
 </body>
 
