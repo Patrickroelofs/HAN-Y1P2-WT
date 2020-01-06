f2edb8aeb96518a0ae4ac710d3e4444118bb41af
Patrick Roelofs <stolenbows@gmail.com>
2020-01-06 11:50:03 +0100

    start of editing project

---
+++ admin/editproject.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['uid'])){
+	header('Location: index.php');
+}
+$thisPage = 'AdminEditProject';
+
+$stmt = $connection->prepare("SELECT * FROM projects WHERE id = '{$_GET['project']}'");
+$stmt->execute();
+
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Admin | Patrick Roelofs</title>
+
+	<?php include('imports/head.php'); ?>
+</head>
+<body>
+<?php include('../includes/adminBar.inc.php'); ?>
+<?php include('imports/navigation.php'); ?>
+
+<main class="container container--large">
+	<form id="loginform" action="includes/editproject.inc.php" method="POST" enctype="multipart/form-data">
+		<fieldset>
+			<div>
+				<label>Title</label>
+				<input name="title" type="text">
+			</div>
+
+			<div>
+				<label>Tags</label>
+				<input name="tags" type="text">
+			</div>
+
+			<div>
+				<label>Date</label>
+				<input name="date" type="date">
+			</div>
+
+			<div>
+				<label>Description</label>
+				<input name="description" type="text">
+			</div>
+
+			<div>
+				<label>Thumbnail</label>
+				<input id="thumbnail" name="thumbnail" type="file">
+			</div>
+
+			<div>
+				<label>Content</label>
+				<textarea name="content"></textarea>
+			</div>
+
+			<div class="center">
+				<button name="addproject-submit" class="button" type="submit">Edit Project</button>
+			</div>
+		</fieldset>
+	</form>
+</main>
+
+<?php include('imports/footer.php'); ?>
+</body>
+</html>
