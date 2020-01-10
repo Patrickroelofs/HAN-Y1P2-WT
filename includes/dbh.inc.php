e816bb510248f2387e77f7e35a065063d5e8aa77
Patrick Roelofs <stolenbows@gmail.com>
2020-01-10 16:44:21 +0100

    XSS Fix

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -10,6 +10,10 @@ try {
     $connection = new PDO("mysql:host=$servername:3306;dbname=$database", $username, $password);
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    /* Stop XSS input and output */
+    $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
 } catch (PDOException $e) {
 
     echo "Connection to database failed: " . $e->getMessage();
