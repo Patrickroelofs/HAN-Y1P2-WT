776d97c74721510dc0436aba30e4a46a9807098a
Patrick Roelofs <stolenbows@gmail.com>
2020-01-15 13:32:55 +0100

    Update DATABASE.sql

--- admin/DATABASE.sql
+++ admin/DATABASE.sql
@@ -26,15 +26,6 @@ CREATE TABLE blog
 
 INSERT INTO portfolio.blog (id, thumbnail, date, title, tags, description, content) VALUES (6, 'images/thumbnails/blog/Cats,anamazingmo/1341233255.jpeg', '2020-01-12', 'Cats, an amazing movie or a shitty piece of CGI?', 'Movie', 'Cats, not my piece of cake...', 'Miaow then turn around and show you my bum i like cats because they are fat and fluffy loved it, hated it, loved it, hated it. &#13;&#10;&#13;&#10;Hunt by meowing loudly at 5am next to human slave food dispenser soft kitty warm kitty little ball of furr yet love i heard this rumor where the humans are our owners, pfft, what do they know?! wake up wander around the house making large amounts of noise jump on top of your human&#39;s bed and fall asleep again. Lick the curtain just to be annoying meowing non stop for food furrier and even more furrier hairball get my claw stuck in the dog&#39;s ear cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters&#39; slippers but cat slap dog in face ooooh feather moving feather!.');
 INSERT INTO portfolio.blog (id, thumbnail, date, title, tags, description, content) VALUES (7, 'images/thumbnails/blog/Science,isitreal/176512609.jpg', '2020-02-12', 'Science, is it real?', 'science', 'The strength of Science and its online journal sites rests with the strengths of its community of authors', 'Besides, you look good in a dress. What&#39;s a knock-out like you doing in a computer-generated gin joint like this? Did you come here for something in particular or just general Riker-bashing? Then maybe you should consider this: if anything happens to them, Starfleet is going to want a full investigation. Congratulations - you just destroyed the Enterprise. Smooth as an android&#39;s bottom, eh, Data? Wait a minute - you&#39;ve been declared dead. You can&#39;t give orders around here. I&#39;ve had twelve years to think about it. And if I had it to do over again, I would have grabbed the phaser and pointed it at you instead of them. The Federation&#39;s gone; the Borg is everywhere!');
-CREATE TABLE experiences
-(
-	id          INT        NOT NULL,
-	location    TINYTEXT   NOT NULL,
-	title       TINYTEXT   NOT NULL,
-	url         TINYTEXT   NOT NULL,
-	date        TINYTEXT   NOT NULL,
-	description MEDIUMTEXT NOT NULL
-);
 
 
 CREATE TABLE messages
